1104c1a28a6b313ee47480f795cc8cc0ca3f9d75
Adam Goddard <agoddard@mercadona.es>
2022-03-30 17:49:30 +0200

    Added attack method and red test for healing

--- iteration_one/RPG.test.js
+++ iteration_one/RPG.test.js
@@ -1,15 +1,57 @@
-it('test create character', () => {
+const { createCharacter, isAlive } = require('./RPG')
+
+it('should can create character', () => {
     const character = createCharacter()
 
     expect(character['health']).toBe(1000)
     expect(character['level']).toBe(1)
 })
 
-it('test character status', () => {
-    const character = createCharacter()
-    const cadaver = createCharacter()
-    cadaver['health'] = 0
+it('should one character attack another one', () => {
+    const characterOne = createCharacter()
+    const characterTwo = createCharacter()
 
-    expect(isAlive(character)).toBeTruthy()
-    expect(isAlive(cadaver)).toBeFalsy()
+    characterOne.attack(characterTwo, 50)
+
+    expect(characterTwo['health']).toBe(950)
+})
+
+it('should one character heal another one', () => {
+    const characterOne = createCharacter()
+    const characterTwo = createCharacter()
+    characterTwo['health'] = 500
+
+    characterOne.heal(characterTwo, 50)
+
+    expect(characterTwo['health']).toBe(550)
+})
+
+it('should one character heal another one to a maximum', () => {
+    const characterOne = createCharacter()
+    const characterTwo = createCharacter()
+    characterTwo['health'] = 500
+
+    characterOne.heal(characterTwo, 550)
+
+    expect(characterTwo['health']).toBe(1000)
+})
+
+describe('character status', () => {
+    it('should check if character is alive', () => {
+        const character = createCharacter()
+        const cadaver = createCharacter()
+        cadaver['health'] = 0
+
+        expect(isAlive(character)).toBeTruthy()
+        expect(isAlive(cadaver)).toBeFalsy()
+    })
+
+    it('should check if character is dead', () => {
+        const characterOne = createCharacter()
+        const characterTwo = createCharacter()
+
+        characterOne.attack(characterTwo, 1000)
+
+        expect(isAlive(characterTwo)).toBeFalsy()
+    })
 })
