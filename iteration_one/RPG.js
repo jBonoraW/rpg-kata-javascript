fcf8a0b1c121d7ec10f068cda10f9779b26386ee
Adam Goddard <agoddard@mercadona.es>
2022-03-31 12:51:08 +0200

    refactoring

--- iteration_one/RPG.js
+++ iteration_one/RPG.js
@@ -1,22 +1,26 @@
+const MAX_HEALTH = 1000
+const MIN_HEALTH = 0
+const STARTING_LEVEL = 1
+
 function createCharacter() {
   return {
-    health: 1000,
-    level: 1,
+    health: MAX_HEALTH,
+    level: STARTING_LEVEL,
     attack: attack,
     heal: heal
   }
 }
 
 function isAlive(character) {
-  return character.health === 0 ? false : true
+  return character.health === MIN_HEALTH ? false : true
 }
 
 function attack(character, damage) {
-  character['health'] = Math.max(character['health'] - damage, 0)
+  character.health = Math.max(character.health - damage, MIN_HEALTH)
 }
 
 function heal(character, healPower) {
-  character['health'] = Math.min(character['health'] + healPower, 1000)
+  character.health = Math.min(character.health + healPower, MAX_HEALTH)
 }
 
 module.exports = {
