892ce01e41993490cc964ffa0b4a1a85b92676a9
Adam Goddard <agoddard@mercadona.es>
2022-03-31 12:45:29 +0200

    linting

--- iteration_one/RPG.js
+++ iteration_one/RPG.js
@@ -1,25 +1,25 @@
 function createCharacter() {
-    return {
-      health: 1000,
-      level: 1,
-      attack: attack,
-      heal: heal
-    }
+  return {
+    health: 1000,
+    level: 1,
+    attack: attack,
+    heal: heal
   }
+}
 
-  function isAlive(character) {
-    return character.health === 0 ? false : true
-  }
+function isAlive(character) {
+  return character.health === 0 ? false : true
+}
 
-  function attack(character, damage) {
-     character['health'] = Math.max(character['health'] - damage, 0)
-  }
+function attack(character, damage) {
+  character['health'] = Math.max(character['health'] - damage, 0)
+}
 
-  function heal(character, healPower) {
-     character['health'] = Math.min(character['health'] + healPower, 1000)
-  }
+function heal(character, healPower) {
+  character['health'] = Math.min(character['health'] + healPower, 1000)
+}
 
-  module.exports = {
-    createCharacter,
-    isAlive,
-  }
+module.exports = {
+  createCharacter,
+  isAlive,
+}
